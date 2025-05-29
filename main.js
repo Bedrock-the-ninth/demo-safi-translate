6234e6294c4186d5a602da5492831fc5bbb721fb
Behzad Ravi <behzad015023@gmail.com>
2025-05-29 19:20:08 +0330

    Fixed conflicts between main and master.

--- main.js
+++ main.js
@@ -1,62 +1,3 @@
-<<<<<<< HEAD
-const canvas = document.getElementById("particle-canvas");
-const ctx = canvas.getContext("2d");
-
-function resizeCanvas() {
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
-}
-resizeCanvas();
-window.addEventListener("resize", resizeCanvas);
-
-class Particle {
-  constructor() {
-    this.reset();
-  }
-  reset() {
-    this.x = Math.random() * canvas.width;
-    this.y = Math.random() * canvas.height;
-    this.vx = (Math.random() - 0.5) * 2;
-    this.vy = (Math.random() - 0.5) * 2;
-    this.size = Math.random() * 3 + 1;
-    this.alpha = Math.random() + 0.5;
-  }
-  update() {
-    this.x += this.vx;
-    this.y += this.vy;
-    if (
-      this.x < 0 ||
-      this.x > canvas.width ||
-      this.y < 0 ||
-      this.y > canvas.height
-    ) {
-      this.reset();
-    }
-  }
-  draw() {
-    ctx.fillStyle = `rgba(0, 135, 158, ${this.alpha})`;
-    ctx.beginPath();
-    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 10);
-    ctx.fill();
-  }
-}
-
-const particles = Array.from({ length: 100 }, () => new Particle());
-
-function animate() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  particles.forEach((p) => {
-    p.update();
-    p.draw();
-  });
-  requestAnimationFrame(animate);
-}
-
-animate();
-
-
-
-=======
 const navigationBar = document.querySelector(".navbar");
 const navbarToggler = document.querySelector(".navbar-toggler");
 const mainPart = document.querySelector("#mainNavbar");
@@ -119,4 +60,3 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   }
 });
->>>>>>> master
