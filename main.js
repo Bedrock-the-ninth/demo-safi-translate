7968b1c588d01b37da8e1b1d02a677edaa6c9b3f
Behzad Ravi <behzad015023@gmail.com>
2025-05-29 18:17:44 +0330

    Don't care to explain, it just works man.

--- main.js
+++ main.js
@@ -4,6 +4,9 @@ const mainPart = document.querySelector("#mainNavbar");
 
 const scrollThreshhold = 50;
 
+const landingCarousel = document.getElementById("landing-carousel");
+
+// Helper Functions
 let scrollHandle = () => {
   if (window.scrollY > scrollThreshhold) {
     navigationBar.classList.add("scrolled");
@@ -14,14 +17,46 @@ let scrollHandle = () => {
 
 let toggleHandle = () => {
   let screenWidth = window.innerWidth;
-  if (!navbarToggler.classList.contains("collapsed") && !navigationBar.classList.contains("scrolled") && screenWidth < 992) {
+  if (
+    !navbarToggler.classList.contains("collapsed") &&
+    !navigationBar.classList.contains("scrolled") &&
+    screenWidth < 992
+  ) {
     mainPart.classList.add("navbar-toggled-bg");
   } else {
     mainPart.classList.remove("navbar-toggled-bg");
   }
 };
 
-window.addEventListener("scroll", scrollHandle);
-navbarToggler.addEventListener("click", toggleHandle);
+let carouselCaptionHandle = () => {
+  const carouselCaptions = document.querySelectorAll(".carousel-caption");
 
-document.addEventListener("DOMContentLoaded", scrollHandle);
+  carouselCaptions.forEach((caption) => {
+    caption.classList.add("d-none");
+  });
+
+  const activeCarouselItem = landingCarousel
+    ? landingCarousel.querySelector(".carousel-item.active")
+    : null;
+
+  if (activeCarouselItem) {
+    const activeCaption = activeCarouselItem.querySelector(".carousel-caption");
+
+    if (activeCaption) {
+      activeCaption.classList.remove("d-none");
+    }
+  }
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  scrollHandle();
+  window.addEventListener("scroll", scrollHandle);
+  navbarToggler.addEventListener("click", toggleHandle);
+
+  carouselCaptionHandle();
+  if (landingCarousel) {
+    landingCarousel.addEventListener("slid.bs.carousel", function (event) {
+      carouselCaptionHandle();
+    });
+  }
+});
